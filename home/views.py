0198fca2ace36eb1110e654523d1e08a45a82feb
AnilKumar Prajapati <iam.anil.25.11@gmail.com>
2024-09-14 23:54:14 +0530

    Home page

--- home/views.py
+++ home/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from .models import Carousel
+from data.models import Video,Image
 # Create your views here.
 def homepage(request):
     user=request.session.get('username')
     img_c=Carousel.objects.all()
     total = range(img_c.count())
-    context={
+    videos_title=Video.objects.all()
+    images_title=Image.objects.all()
+    data={
         'img_c':img_c,
         'total':total,
-        'user':user,                     
+        'user':user,
+        'videos':videos_title,
+        'images':images_title,                  
     }
-    return render(request,'base.html',context)
+    return render(request,'base.html',data)#,context=data)
 
 def important_links(request):
     return render(request,'home/links.html')
